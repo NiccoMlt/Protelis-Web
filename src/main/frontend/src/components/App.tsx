5a666d88e607aa1caf578f4dabb43425204e1fa2
Niccolò Maltoni <nicco.mlt@gmail.com>
2019-11-02 21:45:01 +0100

    feat(frontend): add AppBar component

--- src/main/frontend/src/components/App.tsx
+++ src/main/frontend/src/components/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Greeter from './Greeter';
+import ProtelisAppBar from './AppBar';
 
 const App: React.FC = () => (
   <Router>
     <div className="App">
       <header className="App-header">
-        <Greeter />
+        <ProtelisAppBar />
       </header>
     </div>
   </Router>
