c45857c0903145ebb616bce2d6b41230d6d01e4c
Niccolò Maltoni <nicco.mlt@gmail.com>
2019-11-03 18:54:26 +0100

    fix(frontend): fix Konva canvas test rendering problem
    specify in Jest test that Konva canvas is executed in browser jsdom for
    compatibility problems
    see
    https://github.com/konvajs/react-konva/issues/270#issuecomment-431114761

--- src/main/frontend/src/__test__/App.test.tsx
+++ src/main/frontend/src/__test__/App.test.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme';
+import Konva from 'konva';
 import App from '../components/App';
 
 describe('App component', () => {
   it('renders without crashing inside shallow', () => {
+    // Enzyme does not need to set Konva isBrowser undocumented property
     const component = shallow(<App />);
     expect(component).toBeDefined();
   });
 
   it('renders without crashing', () => {
+    (Konva as any).isBrowser = false; // Needed by React default node environment
+    // https://github.com/konvajs/react-konva/issues/270#issuecomment-431114761
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
     ReactDOM.unmountComponentAtNode(div);
