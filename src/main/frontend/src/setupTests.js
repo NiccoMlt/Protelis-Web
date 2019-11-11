45c53abe39b8aebcfa22e943e8cb58b52d1e28e9
Niccolò Maltoni <nicco.mlt@gmail.com>
2019-11-12 00:40:12 +0100

    test(frontend): make console.error and console.warn fail the test

--- src/main/frontend/src/setupTests.js
+++ src/main/frontend/src/setupTests.js
@@ -1,4 +1,12 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
+global.console.warn = (message) => {
+  throw message;
+};
+
+global.console.error = (message) => {
+  throw message;
+};
+
 configure({ adapter: new Adapter() });
