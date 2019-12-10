3b56e1d26ccf32fc2a1c86f800e369c8f1b68a28
Niccolò Maltoni <nicco.mlt@gmail.com>
2019-12-10 12:00:10 +0100

    test(frontend): add Mock Redux Store support
    add Mock Redux Store support for redux-connected components

--- src/main/frontend/src/__test__/App.test.tsx
+++ src/main/frontend/src/__test__/App.test.tsx
@@ -1,8 +1,12 @@
+import configureMockStore from 'redux-mock-store';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme';
 import Konva from 'konva';
+import { Provider } from 'react-redux';
 import App from '../app/App';
+
+const mockStore = configureMockStore();
 
 describe('App component', () => {
   it('renders without crashing inside shallow', () => {
@@ -15,8 +19,16 @@ describe('App component', () => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     (Konva as any).isBrowser = false; // Needed by React default node environment
     // https://github.com/konvajs/react-konva/issues/270#issuecomment-431114761
+
     const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+    const store = mockStore({ editor: { files: [], open: null } });
+
+    ReactDOM.render(
+      <Provider store={store}>
+        <App />
+      </Provider>,
+      div,
+    );
     ReactDOM.unmountComponentAtNode(div);
   });
 });
