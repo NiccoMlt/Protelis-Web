5eb00b68c8f435400cf09436f282f3eb71217420
Niccolò Maltoni <nicco.mlt@gmail.com>
2019-12-17 10:30:22 +0100

    build(backend): upgrade Node, NPM & Yarn

--- buildSrc/src/main/kotlin/MyVersions.kt
+++ buildSrc/src/main/kotlin/MyVersions.kt
@@ -7,10 +7,10 @@ val Versions.jdk_version: String
   get() = Versions.java_version.toString()
 
 val Versions.node_version: String
-  get() = "10.15.3"
+  get() = "12.13.1"
 
 val Versions.npm_version: String
-  get() = "6.4.1"
+  get() = "6.13.2"
 
 val Versions.yarn_version: String
-  get() = "1.9.4"
+  get() = "1.21.1"
