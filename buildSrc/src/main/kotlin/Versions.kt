687651ab2327231dc4cb9c1d5e46e2f8ab1c70f8
Niccolò Maltoni <nicco.mlt@gmail.com>
2020-01-25 17:13:28 +0100

    build(backend): improved dependencies
    enforce Jackson versions, add Alchemist and remove KotlinLogging
    
    BREAKING CHANGE: Kotlin Logging was removed

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -10,13 +10,21 @@ import kotlin.String
  * YOU are responsible for updating manually the dependency version.
  */
 object Versions {
+  const val com_fasterxml_jackson_core: String = "2.10.2"
+
   const val org_jetbrains_kotlin: String = "1.3.61"
 
   const val org_jetbrains_dokka: String = "0.10.0"
 
-  const val org_junit_jupiter: String = "5.5.2"
+  const val it_unibo_alchemist: String = "9.3.0"
 
-  const val io_vertx: String = "3.8.4"
+  const val org_junit_jupiter: String = "5.6.0"
+
+  const val io_vertx: String = "3.8.5"
+
+  const val ch_qos_logback_logback_classic: String = "1.2.3"
+
+  const val org_slf4j_slf4j_api: String = "1.7.30"
 
   const val com_github_johnrengelman_shadow_gradle_plugin: String = "5.2.0"
 
@@ -32,16 +40,12 @@ object Versions {
 
   const val io_vertx_vertx_plugin_gradle_plugin: String = "1.0.1"
 
-  const val logback_classic: String = "1.2.3"
-
-  const val kotlin_logging: String = "1.7.8"
-
-  const val slf4j_api: String = "1.7.30"
+  const val jackson_module_kotlin: String = "2.10.2"
 
   const val ktlint: String = "0.36.0"
 
   /**
-   * Current version: "5.6.3"
+   * Current version: "6.0.1"
    * See issue 19: How to update Gradle itself?
    * https://github.com/jmfayard/buildSrcVersions/issues/19
    */
