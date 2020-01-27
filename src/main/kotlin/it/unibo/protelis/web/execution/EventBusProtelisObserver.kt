28481bba6a7c9d221c22f73cafc2b287b4fb8f02
Niccolò Maltoni <nicco.mlt@gmail.com>
2020-01-27 10:41:25 +0100

    feat(backend): dummy AlchemistVerticle implemented

--- src/main/kotlin/it/unibo/protelis/web/execution/EventBusProtelisObserver.kt
+++ src/main/kotlin/it/unibo/protelis/web/execution/EventBusProtelisObserver.kt
@@ -1,18 +1,30 @@
 package it.unibo.protelis.web.execution
 
 import io.vertx.core.eventbus.EventBus
+import it.unibo.protelis.web.execution.simulated.AlchemistVerticle.Companion.BASE_ADDRESS
+import it.unibo.protelis.web.execution.simulated.AlchemistVerticle.Companion.FINISHED_SUFFIX
+import it.unibo.protelis.web.execution.simulated.AlchemistVerticle.Companion.INITIALIZED_SUFFIX
+import it.unibo.protelis.web.execution.simulated.AlchemistVerticle.Companion.STEP_DONE_SUFFIX
 
-class EventBusProtelisObserver(private val eb: EventBus) : ProtelisObserver {
-
+/**
+ * This observer implementation adapts Protelis Events to publish on [Vert.x Event Bus][EventBus].
+ *
+ * @param eb the EventBus to publish on
+ * @param addressId the unique execution ID to use in EventBus address
+ */
+class EventBusProtelisObserver(
+  private val eb: EventBus,
+  private val addressId: String
+) : ProtelisObserver {
   override fun initialized(update: ProtelisUpdateMessage) {
-    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    eb.publish("$BASE_ADDRESS.$addressId.$INITIALIZED_SUFFIX", update)
   }
 
   override fun stepDone(update: ProtelisUpdateMessage) {
-    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    eb.publish("$BASE_ADDRESS.$addressId.$STEP_DONE_SUFFIX", update)
   }
 
   override fun finished(update: ProtelisUpdateMessage) {
-    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    eb.publish("$BASE_ADDRESS.$addressId.$FINISHED_SUFFIX", update)
   }
 }
