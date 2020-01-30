ca9192d3fca9816a26bee6b4e3d48a9c540b81dc
Niccolò Maltoni <nicco.mlt@gmail.com>
2020-01-30 23:32:06 +0100

    feat(backend): enable async on Protelis engine with coroutines

--- src/main/kotlin/it/unibo/protelis/web/execution/ProtelisEngine.kt
+++ src/main/kotlin/it/unibo/protelis/web/execution/ProtelisEngine.kt
@@ -1,5 +1,7 @@
 package it.unibo.protelis.web.execution
 
+import java.util.concurrent.Future
+
 /** A ProtelisEngine object is some kind of connection with a simulator or actual network of devices. */
 interface ProtelisEngine {
 
@@ -9,22 +11,28 @@ interface ProtelisEngine {
    *
    * @param sourceCode the Protelis code to execute
    * @param monitor an observer object that monitors the execution and models the output strategy
+   *
+   * @return a [Future] that determines if the setup was successful or not
    */
-  fun setup(sourceCode: String, monitor: ProtelisObserver)
+  fun setup(sourceCode: String, monitor: ProtelisObserver): Future<Unit>
 
   /**
    * Start the execution of the code.
    *
+   * @return a [Future] that determines if the starting was successful or not
+   *
    * @throws IllegalStateException if the connection was not set up (use [setup] method)
    */
-  fun start()
+  fun start(): Future<Unit>
 
   /**
    * Stop and tear down the connection.
    *
+   * @return a [Future] that determines if the starting was successful or not
+   *
    * @throws IllegalStateException if the connection was not set up (use [setup] method)
    */
-  fun stop()
+  fun stop(): Future<Unit>
 
   // TODO: enable some kind of interaction
 }
--- src/main/kotlin/it/unibo/protelis/web/execution/simulated/SimulatedProtelisEngine.kt
+++ src/main/kotlin/it/unibo/protelis/web/execution/simulated/SimulatedProtelisEngine.kt
@@ -2,44 +2,120 @@ package it.unibo.protelis.web.execution.simulated
 
 import it.unibo.alchemist.core.implementations.Engine
 import it.unibo.alchemist.core.interfaces.Simulation
-import it.unibo.alchemist.loader.Loader
+import it.unibo.alchemist.core.interfaces.Status
 import it.unibo.alchemist.loader.YamlLoader
 import it.unibo.alchemist.model.implementations.positions.Euclidean2DPosition
 import it.unibo.alchemist.model.implementations.times.DoubleTime
-import it.unibo.protelis.web.execution.ProtelisEngine
+import it.unibo.alchemist.model.interfaces.Position
+import it.unibo.protelis.web.execution.CoroutineProtelisEngine
 import it.unibo.protelis.web.execution.ProtelisObserver
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.atomic.AtomicReference
 
-class SimulatedProtelisEngine() : ProtelisEngine {
-  private var alchemistEngine: Simulation<Any /* TODO */, Euclidean2DPosition>? = null
+class SimulatedProtelisEngine() : CoroutineProtelisEngine() {
+  private val logger: Logger = LoggerFactory.getLogger(this::class.java)
+  private var alchemistEngine: AtomicReference<Simulation<Any /* TODO */, Euclidean2DPosition>?> = AtomicReference(null)
+
+  init {
+    logger.debug("Simulation container created")
+  }
 
   constructor(sourceCode: String, monitor: ProtelisObserver) : this() {
-    setup(sourceCode, monitor)
+    GlobalScope.launch { setupAwait(sourceCode, monitor) }
   }
 
-  override fun setup(sourceCode: String, monitor: ProtelisObserver) {
-    if (alchemistEngine != null) stop()
-    setupSimulation()
-    alchemistEngine?.addOutputMonitor(ProtelisUpdateAdapter(monitor))
+  override suspend fun setupAwait(sourceCode: String, monitor: ProtelisObserver) {
+    if (alchemistEngine.get() != null) stopAwait()
+    val sim = setupSimulationAsync().await()
+    alchemistEngine.set(sim)
+    setCode(sourceCode)
+    alchemistEngine.get()?.addOutputMonitor(ProtelisUpdateAdapter(monitor))
+    alchemistEngine.get()?.awaitFor(Status.READY) ?: throw IllegalStateException("Simulation was not set up correctly")
+    logger.debug("Simulation Engine set up correctly")
   }
 
-  override fun start() {
-    checkNotNull(alchemistEngine)
-    alchemistEngine?.play()
+  override suspend fun startAwait() {
+    checkNotNull(alchemistEngine.get())
+    alchemistEngine.get()?.play()
+    alchemistEngine.get()?.awaitFor(Status.RUNNING)
+    logger.debug("Simulation started")
   }
 
-  override fun stop() {
-    checkNotNull(alchemistEngine)
-    alchemistEngine?.terminate()
-    // alchemistEngine?.waitFor() // TODO: enable coroutine await for simulator termination
-    alchemistEngine = null
+  override suspend fun stopAwait() {
+    checkNotNull(alchemistEngine.get())
+    alchemistEngine.get()?.terminate()
+    alchemistEngine.get()?.awaitFor(Status.TERMINATED)
+    alchemistEngine.set(null)
+    logger.debug("Simulation stopped and deleted")
   }
 
-  private fun setupSimulation() {
-    val loader: Loader = YamlLoader(this.javaClass.classLoader.getResourceAsStream("simulation.yml"))
-    alchemistEngine = Engine<Any /* TODO */, Euclidean2DPosition>(
+  /**
+   * Asynchronously build a new simulation from a loader of YAML files.
+   */
+  private fun setupSimulationAsync(): Deferred<Engine<Any, Euclidean2DPosition>> = GlobalScope.async {
+    val loader = YamlLoader(this.javaClass.classLoader.getResourceAsStream("simulation.yml"))
+    loader.getDefault<Any /* TODO */, Euclidean2DPosition>()
+    Engine<Any /* TODO */, Euclidean2DPosition>(
       loader.getDefault(),
       Long.MAX_VALUE,
       DoubleTime(Double.POSITIVE_INFINITY)
     )
   }
+
+  private fun setCode(sourceCode: String) {
+    alchemistEngine
+      .get()
+      ?.environment
+      ?.nodes
+      ?.forEach { node ->
+        node
+          .contents
+          .filterKeys { it.name == "_to_exec" }
+          .forEach { (mol, _) ->
+            node.setConcentration(mol, sourceCode)
+            logger.debug("Injecting Protelis code to molecule \"$mol\" of node \"${node.id}\"")
+          }
+      }
+      ?: throw IllegalStateException("Can't set code to non-existent simulation")
+  }
 }
+
+/**
+ * Asynchronous wait for the simulation to reach the selected [Status].
+ *
+ * @param s The [Status] the simulation must reach before returning from this method
+ * @param timeout The maximum lapse of time the caller wants to wait before being resumed (0 means "no limit")
+ * @param timeunit The [TimeUnit] used to define "timeout"
+ *
+ * @return the [Deferred] [Status] of the Simulation at the end of the wait
+ *
+ * @throws InterruptedException if the wait somehow crashes or if time runs out
+ */
+fun <T, P : Position<out P>> Simulation<T, P>.waitForAsync(
+  s: Status,
+  timeout: Long = 0,
+  timeunit: TimeUnit = TimeUnit.MILLISECONDS
+): Deferred<Status> = GlobalScope.async { waitFor(s, timeout, timeunit) }
+
+/**
+ * Suspended function that waits for the simulation to reach the selected [Status].
+ *
+ * @param s The [Status] the simulation must reach before returning from this method
+ * @param timeout The maximum lapse of time the caller wants to wait before being resumed (0 means "no limit")
+ * @param timeunit The [TimeUnit] used to define "timeout"
+ *
+ * @return the [Status] of the Simulation at the end of the wait
+ *
+ * @throws InterruptedException if the wait somehow crashes or if time runs out
+ */
+suspend fun <T, P : Position<out P>> Simulation<T, P>.awaitFor(
+  s: Status,
+  timeout: Long = 0,
+  timeunit: TimeUnit = TimeUnit.MILLISECONDS
+): Status = waitForAsync(s, timeout, timeunit).await()
