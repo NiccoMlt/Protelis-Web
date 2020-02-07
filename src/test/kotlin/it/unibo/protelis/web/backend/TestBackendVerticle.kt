0ea98b8f061e3703afc7dd729b5a197a6e4ca88f
Niccolò Maltoni <nicco.mlt@gmail.com>
2020-02-07 17:57:11 +0100

    test(backend): add and improve tests

--- src/test/kotlin/it/unibo/protelis/web/backend/TestBackendVerticle.kt
+++ src/test/kotlin/it/unibo/protelis/web/backend/TestBackendVerticle.kt
@@ -4,19 +4,16 @@ import io.vertx.core.Vertx
 import io.vertx.junit5.VertxExtension
 import io.vertx.junit5.VertxTestContext
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 
 @ExtendWith(VertxExtension::class)
 class TestBackendVerticle {
 
-  @BeforeEach
-  fun deploy_verticle(vertx: Vertx, testContext: VertxTestContext) {
-    vertx.deployVerticle(BackendVerticle(), testContext.succeeding<String> { testContext.completeNow() })
-  }
-
   @Test
-  fun verticle_deployed(@Suppress("UNUSED_PARAMETER") vertx: Vertx, testContext: VertxTestContext) {
-    testContext.completeNow()
+  @DisplayName("Test BackendVerticle deploy")
+  fun `deploy BackendVerticle`(vertx: Vertx, testContext: VertxTestContext) {
+    vertx.deployVerticle(BackendVerticle(), testContext.succeeding<String> { testContext.completeNow() })
   }
 }
