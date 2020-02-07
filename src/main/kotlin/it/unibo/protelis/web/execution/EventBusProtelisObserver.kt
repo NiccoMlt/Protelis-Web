68120bbe9bce027a16095294b2a4a8f9e2b7cf71
Niccolò Maltoni <nicco.mlt@gmail.com>
2020-02-07 18:19:43 +0100

    test(backend): add engine tests

--- src/main/kotlin/it/unibo/protelis/web/execution/EventBusProtelisObserver.kt
+++ src/main/kotlin/it/unibo/protelis/web/execution/EventBusProtelisObserver.kt
@@ -1,12 +1,7 @@
 package it.unibo.protelis.web.execution
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import io.vertx.core.eventbus.EventBus
-import io.vertx.core.json.JsonObject
 import io.vertx.core.json.JsonObject.mapFrom
-import io.vertx.ext.web.impl.Utils
-import io.vertx.kotlin.core.json.json
 import it.unibo.protelis.web.execution.simulated.AlchemistVerticle.Companion.finishedAddress
 import it.unibo.protelis.web.execution.simulated.AlchemistVerticle.Companion.initializedAddress
 import it.unibo.protelis.web.execution.simulated.AlchemistVerticle.Companion.stepDoneAddress
@@ -26,17 +21,17 @@ class EventBusProtelisObserver(
   private val logger: Logger = LoggerFactory.getLogger(this::class.java)
 
   override fun initialized(update: ProtelisUpdateMessage) {
-    logger.debug("Handle init update: $update")
+    logger.trace("Handle init update: $update")
     eb.publish(initializedAddress(addressId), mapFrom(update))
   }
 
   override fun stepDone(update: ProtelisUpdateMessage) {
-    logger.debug("Handle step update: $update")
+    logger.trace("Handle step update: $update")
     eb.publish(stepDoneAddress(addressId), mapFrom(update))
   }
 
   override fun finished(update: ProtelisUpdateMessage) {
-    logger.debug("Handle end update: $update")
+    logger.trace("Handle end update: $update")
     eb.publish(finishedAddress(addressId), mapFrom(update))
   }
 }
---
+++ src/test/kotlin/it/unibo/protelis/web/execution/simulated/SimulatedProtelisEngineTest.kt
@@ -0,0 +1,47 @@
+package it.unibo.protelis.web.execution.simulated
+
+import it.unibo.protelis.web.execution.ProtelisObserver
+import it.unibo.protelis.web.execution.ProtelisUpdateMessage
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import kotlin.test.assertTrue
+
+@DisplayName("Test simulated Protelis engine with Alchemist")
+class SimulatedProtelisEngineTest {
+
+  @Test
+  @DisplayName("Test that injected code works")
+  fun testCodeInject() {
+    val simulation = SimulatedProtelisEngine()
+    var init = false
+    var step = 0
+    var done = false
+    val monitor = object : ProtelisObserver {
+      override fun initialized(update: ProtelisUpdateMessage) {
+        init = true
+      }
+
+      override fun stepDone(update: ProtelisUpdateMessage) {
+        println("Step done")
+        step++
+      }
+
+      override fun finished(update: ProtelisUpdateMessage) {
+        done = true
+      }
+    }
+    simulation.setup(
+      """
+        def aFunction() = 1
+        aFunction() * self.nextRandomDouble()
+      """.trimIndent(),
+      monitor
+    ).join()
+    simulation.start().join()
+    Thread.sleep(1000)
+    simulation.stop().join()
+    assertTrue(init, "Simulation should have been initialized")
+    assertTrue(step > 0, "Simulation should have run at least one step")
+    assertTrue(done, "Simulation should have been terminated")
+  }
+}
