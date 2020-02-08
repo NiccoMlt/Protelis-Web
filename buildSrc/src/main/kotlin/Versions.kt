128000640afa3379c9b122bdefa91bf02c87a2a4
Niccolò Maltoni <nicco.mlt@gmail.com>
2020-02-08 16:58:48 +0100

    build(backend): reconfigure test deps

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -24,13 +24,17 @@ object Versions {
 
   const val io_vertx: String = "3.8.5"
 
+  const val ch_qos_logback_logback_classic: String = "1.2.3"
+
+  const val org_slf4j_slf4j_api: String = "1.7.30"
+
   const val com_github_johnrengelman_shadow_gradle_plugin: String = "5.2.0"
 
   const val org_jlleitschuh_gradle_ktlint_gradle_plugin: String = "9.1.1"
 
   const val com_github_node_gradle_node_gradle_plugin: String = "2.2.1"
 
-  const val de_fayard_refreshversions_gradle_plugin: String = "0.7.0" // available: "0.8.6"
+  const val de_fayard_refreshversions_gradle_plugin: String = "0.7.0"
 
   const val org_jetbrains_kotlin_kapt_gradle_plugin: String = "1.3.61"
 
@@ -50,11 +54,7 @@ object Versions {
 
   const val kotlinx_coroutines_io: String = "0.24.0"
 
-  const val junit_vintage_engine: String = "5.6.0"
-
-  const val logback_classic: String = "1.3.0-alpha4"
-
-  const val slf4j_api: String = "1.8.0-beta2"
+  const val jul_to_slf4j: String = "1.7.30"
 
   const val ktlint: String = "0.36.0"
 
