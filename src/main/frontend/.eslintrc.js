3fe593928fdf2b43df917866bfa1c25cb7a3bd8d
Restyled.io <commits@restyled.io>
2020-02-08 15:19:03 +0000

    Restyled by prettier

--- src/main/frontend/.eslintrc.js
+++ src/main/frontend/.eslintrc.js
@@ -1,29 +1,28 @@
 module.exports = {
-  extends : [
-    "airbnb-typescript", "airbnb/hooks",
+  extends: [
+    "airbnb-typescript",
+    "airbnb/hooks",
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking"
   ],
-  parserOptions : {
-    "project" : "./tsconfig.json",
-    "tsconfigRootDir" : ".",
+  parserOptions: {
+    project: "./tsconfig.json",
+    tsconfigRootDir: "."
   },
-  plugins : [ "tsdoc" ],
-  rules : {
-    "react/prop-types" : "off",
-    "react/jsx-props-no-spreading" : [
+  plugins: ["tsdoc"],
+  rules: {
+    "react/prop-types": "off",
+    "react/jsx-props-no-spreading": ["warn", { html: "enforce", custom: "ignore", explicitSpread: "ignore" }],
+    "@typescript-eslint/explicit-function-return-type": [
       "warn",
-      {"html" : "enforce", "custom" : "ignore", "explicitSpread" : "ignore"}
-    ],
-    "@typescript-eslint/explicit-function-return-type" : [
-      "warn", {
-        allowExpressions : true,
+      {
+        allowExpressions: true
       }
     ],
-    "no-param-reassign" : [ "warn", {"props" : false} ],
-    "tsdoc/syntax" : "warn",
-    "max-len" : [ "error", {"code" : 120, "tabWidth" : 4} ],
-    "no-console" : [ "warn", {allow : [ "warn", "error" ]} ]
+    "no-param-reassign": ["warn", { props: false }],
+    "tsdoc/syntax": "warn",
+    "max-len": ["error", { code: 120, tabWidth: 4 }],
+    "no-console": ["warn", { allow: ["warn", "error"] }]
   }
 };
--- src/main/frontend/src/setupTests.js
+++ src/main/frontend/src/setupTests.js
@@ -1,16 +1,20 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */ // it's a
-                                                                      // JS file
+// JS file
 /* eslint-disable @typescript-eslint/unbound-method */ // Jest wants it like
-                                                       // that
+// that
 /* eslint-disable import/no-extraneous-dependencies */ // testing-library is not
-                                                       // a runtime dep
+// a runtime dep
 
-import '@testing-library/jest-dom/extend-expect';
-import {configure} from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import "@testing-library/jest-dom/extend-expect";
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
-global.console.warn = (message) => { throw message; };
+global.console.warn = message => {
+  throw message;
+};
 
-global.console.error = (message) => { throw message; };
+global.console.error = message => {
+  throw message;
+};
 
-configure({adapter : new Adapter()});
+configure({ adapter: new Adapter() });
