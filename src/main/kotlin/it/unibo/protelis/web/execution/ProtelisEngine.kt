3584143bd17f9e90d839b6493df84a6882ce8901
Niccolò Maltoni <nicco.mlt@gmail.com>
2020-02-21 17:01:40 +0100

    docs: document methods

--- src/main/kotlin/it/unibo/protelis/web/execution/ProtelisEngine.kt
+++ src/main/kotlin/it/unibo/protelis/web/execution/ProtelisEngine.kt
@@ -1,7 +1,5 @@
 package it.unibo.protelis.web.execution
 
-import java.util.concurrent.Future
-
 /** A ProtelisEngine object is some kind of connection with a simulator or actual network of devices. */
 interface ProtelisEngine {
 
@@ -11,28 +9,22 @@ interface ProtelisEngine {
    *
    * @param sourceCode the Protelis code to execute
    * @param monitor an observer object that monitors the execution and models the output strategy
-   *
-   * @return a [Future] that determines if the setup was successful or not
    */
-  fun setup(sourceCode: String, monitor: ProtelisObserver): Future<Unit>
+  fun setup(sourceCode: String, monitor: ProtelisObserver)
 
   /**
    * Start the execution of the code.
    *
-   * @return a [Future] that determines if the starting was successful or not
-   *
    * @throws IllegalStateException if the connection was not set up (use [setup] method)
    */
-  fun start(): Future<Unit>
+  fun start()
 
   /**
    * Stop and tear down the connection.
    *
-   * @return a [Future] that determines if the starting was successful or not
-   *
    * @throws IllegalStateException if the connection was not set up (use [setup] method)
    */
-  fun stop(): Future<Unit>
+  fun stop()
 
   // TODO: enable some kind of interaction
 }
